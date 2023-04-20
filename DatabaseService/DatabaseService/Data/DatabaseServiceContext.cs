06130cd6248da2b2e7767a4f671d0d54e54fee23
7Woker7 <83217918+7Woker7@users.noreply.github.com>
2023-04-20 11:44:48 +0300

    Created two web services

---
+++ DatabaseService/DatabaseService/Data/DatabaseServiceContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DatabaseService.Models;
+
+namespace DatabaseService.Data
+{
+    public class DatabaseServiceContext : DbContext
+    {
+        public DatabaseServiceContext(DbContextOptions<DatabaseServiceContext> opt) : base(opt)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ DatabaseService/DatabaseService/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatabaseService.Models
+
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Hobby { get; set; }
+
+    }
+}
